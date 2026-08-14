12abb65f54561bbfae6989e9d9a6878c22c34df2
Satish Kumar <satishkumarsinnar3520@gmail.com>
2026-08-14 14:05:32 +0530

    initial commit - prodesk it landing page

---
+++ script.js
@@ -0,0 +1,17 @@
+let icon = document.getElementById("icon");
+
+icon.addEventListener("click", function () {
+  document.querySelector("body").classList.toggle("active");
+  if (document.querySelector("body").className == "active") {
+    icon.className = "fa-solid fa-sun"
+  } else {
+    icon.className = "fa-solid fa-moon"
+  }
+})
+
+const bars = document.getElementById("bars");
+const nav = document.getElementById("nav");
+
+bars.addEventListener('click', () => {
+  nav.classList.toggle('show');
+});
